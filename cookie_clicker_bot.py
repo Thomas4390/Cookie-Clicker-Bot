8844bd5c572fd5b728f6715cdc622a632a291059
Thomas Vaudescal <vaudescal.t@gmail.com>
2022-07-02 15:59:58 -0400

    [ADD] | minors changes

--- cookie_clicker_bot.py
+++ cookie_clicker_bot.py
@@ -4,11 +4,15 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service(
-    '/Users/thomasvaudescal/Documents/Mes_documents/22_VSCODE/Cookie-Clicker-Bot/chromedriver')
+path = '/Users/thomasvaudescal/Documents/Mes_documents/22_VSCODE/Cookie-Clicker-Bot/chromedriver'
+
+service = Service(path)
 service.start()
 driver = webdriver.Remote(service.service_url)
-driver.get('https://www.google.com/')
+driver.get('https://orteil.dashnet.org/cookieclicker/v10466/')
 
-time.sleep(5)
+time.sleep(1000)
+
+driver.find_element_by_id('bigCookie').click()
+
 driver.quit()
